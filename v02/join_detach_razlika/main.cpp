0ddb9fe259c55d1e3e0d84a13a6c3ed48e62d689
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-16 10:15:24 +0100

    Update main.cpp

--- v02/join_detach_razlika/main.cpp
+++ v02/join_detach_razlika/main.cpp
@@ -1,7 +1,9 @@
 /*
-Napraviti konkurentni program koji stvara nit iz koje pita korisnika za visinu u centimetrima i potom ispisuje: “Vasa visina je <uneta_visina> cm.”
+Napraviti konkurentni program koji stvara nit iz koje pita korisnika za visinu u centimetrima,
+i potom ispisuje: “Vasa visina je <uneta_visina> cm.”
 
-Testirati program tako što će main nit, koristeći objekat koji predstavlja novu nit, da pozove metodu join. Potom promeniti taj poziv u poziv metode detach.
+Testirati program tako što će main nit, koristeći objekat koji predstavlja novu nit, da pozove metodu join.
+Potom promeniti taj poziv u poziv metode detach.
 */
 
 #include <iostream>
