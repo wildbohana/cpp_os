ef7dbb9204b94b59806a81cd1d8c0cd7370ae095
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:14:55 +0200

    Update main.cpp

--- v02/pokretanje_vise_niti/main.cpp
+++ v02/pokretanje_vise_niti/main.cpp
@@ -1,7 +1,7 @@
 /*
-Napraviti konkurentni program koji pokreće više niti u petlji.
-Svakoj niti proslediti njen redni broj prilikom kreiranja i svaka nit treba 
-da ispiše sopstveni redni broj u okviru tela niti.
+Napraviti konkurentni program koji pokrece vise niti u petlji.
+Svakoj niti proslediti njen redni broj prilikom kreiranja i 
+svaka nit treba da ispise sopstveni redni broj u okviru tela niti.
 */
 
 #include <iostream>
@@ -10,13 +10,14 @@ da ispiše sopstveni redni broj u okviru tela niti.
 using namespace std;
 
 void f(int x) {
-    // niti mogu jedna drugu da prekinu u pola ispisa
+    // Niti mogu jedna drugu da prekinu u pola ispisa
     cout << "Nit: " << x << endl;
 }
 
+const int br_niti = 5;
+
 int main()
 {
-    const int br_niti = 5;
 
     /*
     // nije konkurentno
@@ -25,12 +26,15 @@ int main()
     }
     */
 
+	// Kreiranje niza niti
     thread t[br_niti];
 
+	// Inicijalizacija svih nizi
     for(int i = 0; i < br_niti; i++) {
         t[i] = thread(f, i+1);
     }
 
+	// Cekanje da se sve niti izvrse
     for(int i = 0; i < br_niti; i++) {
         t[i].join();
     }
