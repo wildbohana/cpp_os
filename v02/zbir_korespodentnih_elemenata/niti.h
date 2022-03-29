c38de418ca7b84b4b4bc2d9be051a5bf67644a48
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:24:17 +0200

    Update niti.h

--- v02/zbir_korespodentnih_elemenata/niti.h
+++ v02/zbir_korespodentnih_elemenata/niti.h
@@ -8,13 +8,20 @@
 
 using namespace std;
 
-// v - vektor A čije elemente treba sumirati
-// v - vektor B čije elemente treba sumirati
-// povratna vrednost - suma korespondentnih elemenata vektora A i B, izračunata pozivanjem dve niti
+/*
+v - vektor A cije elemente treba sumirati
+v - vektor B cije elemente treba sumirati
+povratna vrednost - suma korespondentnih elemenata vektora A i B, izracunata pozivanjem dve niti
+*/
+
 vector<double> sumiraj(vector<double> a, vector<double> b) {
+	// Vektor u koji smestamo zbirove parova
     vector<double> c (a.size());
+	
     unsigned polovina = (a.size() % 2 == 0 ? (a.size() / 2) : (a.size() / 2) + 1);
 
+	// void f(ci a_begin, ci a_end, ci b_begin, vector<double>::iterator sum_begin)
+	// t1 sabira korespodentne elemente u prvoj polovini, t2 sabira elemente u drugoj polovini
     thread t1 = thread(f, a.begin(), a.begin() + polovina, b.begin(), c.begin());
     thread t2 = thread(f, a.begin() + polovina, a.end(), b.begin() + polovina, c.begin() + polovina);
 
