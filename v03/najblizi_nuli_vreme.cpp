01885440441141ed77f2030bfb51d03b87139a87
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:30:58 +0200

    Update najblizi_nuli_vreme.cpp

--- v03/najblizi_nuli_vreme.cpp
+++ v03/najblizi_nuli_vreme.cpp
@@ -21,68 +21,68 @@ typedef vector<double>::const_iterator cit;
 
 // Oke ovo je zanimljivo
 struct vreme {
-steady_clock::time_point pocetak;
-steady_clock::time_point kraj;
+    steady_clock::time_point pocetak;
+    steady_clock::time_point kraj;
 };
 
 // Funkcija koja trazi minimum
 void f(cit pocetak, cit kraj, double &min, struct vreme *v) {
-// Uzimanje vremena pre pocetka izvrsavanja
-v->pocetak = steady_clock::now();
+    // Uzimanje vremena pre pocetka izvrsavanja
+    v->pocetak = steady_clock::now();
 
-// Trazenje minimuma
-min = abs(*pocetak);
-for(; pocetak != kraj; pocetak++) {
-if(abs(*pocetak) < abs(min)) {
-    min = *pocetak;
-}
-}
+    // Trazenje minimuma
+    min = abs(*pocetak);
+    for(; pocetak != kraj; pocetak++) {
+        if(abs(*pocetak) < abs(min)) {
+            min = *pocetak;
+        }
+    }
 
-// Uzimanje vremena nakon završetka izvršavanja
-v->kraj = steady_clock::now();
+    // Uzimanje vremena nakon zavrsetka izvrsavanja
+    v->kraj = steady_clock::now();
 }
 
 const int BROJ_NITI = 3;
 const int DUZINA = 900000;
 
 int main() {
-int SEGMENT = DUZINA / BROJ_NITI;
+    int SEGMENT = DUZINA / BROJ_NITI;
 
-// Postavljamo seme random generatora na trenutno vreme, sto dovodi do kvalitetnijeg izbora slucajnih brojeva
-srand(time(NULL));  
+    // Postavljamo seme random generatora na trenutno vreme, sto dovodi do kvalitetnijeg izbora slucajnih brojeva
+    srand(time(NULL));  
 
-// Inicijalizujemo vektor i punimo ga pseudo-slucajnim brojevima
-vector<double> v(DUZINA);
-for (int i = 0; i < DUZINA; i++) {
-v[i] = rand();
-}
+    // Inicijalizujemo vektor i punimo ga pseudo-slucajnim brojevima
+    vector<double> v(DUZINA);
+    for (int i = 0; i < DUZINA; i++) {
+        v[i] = rand();
+    }
 
-// Niz elemenata najblizih nuli - svaka programska nit ce dati svoj medjurezultat
-double minimumi[BROJ_NITI];  
-// Niz struktura u koji ce biti upisani podaci o trajanju izvrsavanja svake niti
-struct vreme vremena[BROJ_NITI]; 
+    // Niz elemenata najblizih nuli - svaka programska nit ce dati svoj medjurezultat
+    double minimumi[BROJ_NITI];  
+    // Niz struktura u koji ce biti upisani podaci o trajanju izvrsavanja svake niti
+    struct vreme vremena[BROJ_NITI]; 
 
-// Potrebne niti
-thread niti[BROJ_NITI];
+    // Potrebne niti
+    thread niti[BROJ_NITI];
 
-// Pokretanje niti
-for(int i = 0; i < BROJ_NITI; i++) {
-niti[i] = thread(f, v.cbegin(), v.cbegin() + (i + 1) * SEGMENT, ref(minimumi[i]), &vremena[i]);
-}
+    // Pokretanje niti
+    for(int i = 0; i < BROJ_NITI; i++) {
+        niti[i] = thread(f, v.cbegin(), v.cbegin() + (i + 1) * SEGMENT, ref(minimumi[i]), &vremena[i]);
+    }
 
-// join i ispis niti
-for(int i = 0; i < BROJ_NITI; i++) {
-niti[i].join();
+    // join i ispis niti
+    for(int i = 0; i < BROJ_NITI; i++) {
+        niti[i].join();
 
-cout << "Nit[" << i << "]" << endl;
-duration<double, milli> dms = vremena[i].kraj - vremena[i].pocetak;
+        cout << "Nit[" << i << "]" << endl;
+        duration<double, milli> dms = vremena[i].kraj - vremena[i].pocetak;
 
-cout << "Trajanje: " << dms.count()  << "ms" << endl;
-cout << "Min[" << i << "] = " << minimumi[i] << endl << endl;
-}
+        cout << "Trajanje: " << dms.count()  << "ms" << endl;
+        cout << "Min[" << i << "] = " << minimumi[i] << endl << endl;
+    }
 
-// Pronalazenje vremena najblizeg nuli
-cout << endl << "Najmanji: " << *(min_element(minimumi, minimumi + 3)) << endl;
+    // Pronalazenje vremena najblizeg nuli
+    cout << endl << "Najmanji: " << *(min_element(minimumi, minimumi + 3)) << endl;
 
-return 0;
+    return 0;
 }
