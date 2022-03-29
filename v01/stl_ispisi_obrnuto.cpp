8945170430f6aa8f1cb94edb9dab533538cf68b9
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 21:41:47 +0200

    Update stl_ispisi_obrnuto.cpp

--- v01/stl_ispisi_obrnuto.cpp
+++ v01/stl_ispisi_obrnuto.cpp
@@ -1,6 +1,6 @@
 /*
-Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i zatim ih ispisuje u obrnutom redosledu 
-(poslednja prihvaćena linija se ispisuje prva).
+Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i zatim 
+ih ispisuje u obrnutom redosledu (poslednja prihvacena linija se ispisuje prva).
 */
 
 #include <iostream>
@@ -10,20 +10,21 @@ Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i z
 using namespace std;
 
 int main() {
-   
     vector<string> linije;
     string linija;
 	
 	cout << "Unesite linije:" << endl;
     
+	// Ucitavanje stringova
 	while (getline(cin, linija)) {
         linije.push_back(linija);
 	}
 
     cout << "Ispis obrnuto:" << endl;
 	
-    for (auto it = linije.rbegin(); it != linije.rend(); it++) {
-        cout << *it << endl;
+	// Za obrnuti ispis koristimo reverse iteratore
+    for (auto it = linije.crbegin(); it != linije.crend(); it++) {
+        cout << "Recenica" << i << ": " << *it << endl;
 	}
 
     return 0;
