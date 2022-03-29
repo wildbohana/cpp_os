e01baac4d9487280c37f09207514e5d38a8b92bf
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:04:58 +0200

    Update stl_vector_min.cpp

--- v01/stl_vector_min.cpp
+++ v01/stl_vector_min.cpp
@@ -15,40 +15,38 @@ Podrazumeva se: a.size()==b.size()
 using namespace std;
 
 // Funkcija koja vraca vektor koji sadrzi minimume korespodentnih elemenata
-vector<int> min(const vector<int> &a, const vector<int> &b)
-{
-    vector<int> c(a.size());
+vector<int> min(const vector<int> &a, const vector<int> &b) {
+	vector<int> c(a.size());
 
-    auto it_a = a.cbegin(), it_b = b.cbegin();
-    auto it_c = c.begin();
+	auto it_a = a.cbegin(), it_b = b.cbegin();
+	auto it_c = c.begin();
 
-    while (it_a != a.cend()) {
-        *it_c++ = std::min(*it_a++, *it_b++);
+	while (it_a != a.cend()) {
+		*it_c++ = std::min(*it_a++, *it_b++);
 	}
 
-    return c;
-}
+	return c;
+	}
 
-int main()
-{
-    vector<int> a = {1, 2, 3, 4, 5};
-    vector<int> b = {4, 3, 7, 3, 2};
+int main() {
+	vector<int> a = {1, 2, 3, 4, 5};
+	vector<int> b = {4, 3, 7, 3, 2};
 
-    vector<int> c = min(a, b);
+	vector<int> c = min(a, b);
 
 	// Ispis rezultata
-    cout << "\nRezultat:";
-    for (int i = 0; i < n; i++) {
-        cout << c[i] << " ";
-    }
-    cout << endl;
+	cout << "\nRezultat:";
+	for (int i = 0; i < n; i++) {
+		cout << c[i] << " ";
+	}
+	cout << endl;
 
 	/*
 	cout << "Ispis rezultata - drugi nacin: " << endl;
-    for (int x : c) {
+	for (int x : c) {
 		cout << x << endl;
 	}
 	*/
 
-    return 0;
+	return 0;
 }
