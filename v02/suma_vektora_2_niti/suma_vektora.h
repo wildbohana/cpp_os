ba6229ee9e2eb6fb3219678e324d3df7a935ce8f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:18:55 +0200

    Update suma_vektora.h

--- v02/suma_vektora_2_niti/suma_vektora.h
+++ v02/suma_vektora_2_niti/suma_vektora.h
@@ -12,8 +12,8 @@ pocetak - iterator na prvi element koji se uzima u obzir pri računanju sume
 kraj - iterator iza poslednjeg elementa koji se uzima u obzir pri računanju sume
 zbir - parameter kroz koji funkcija treba da vrati izračunatu sumu
 */
-void f(ci pocetak, ci kraj, double& zbir) {
-    double zbir = 0;
+void f(ci pocetak, ci kraj, double &zbir) {
+    zbir = 0;
     while (pocetak != kraj) {
         zbir += *pocetak++;
     }
