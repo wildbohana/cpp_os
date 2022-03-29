9d7244630837b550a1b4e162b9d5073f87d53aef
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:18:33 +0200

    Update niti.h

--- v02/suma_vektora_2_niti/niti.h
+++ v02/suma_vektora_2_niti/niti.h
@@ -8,17 +8,26 @@
 
 using namespace std;
 
-// v - vektor čije elemente treba sumirati
-// povratna vrednost - suma svih elemenata vektora, izračunata pokretanjem 2 niti
-// svaka nit treba da obradi jednu polovinu elemenata
+/* 
+v - vektor cije elemente treba sumirati
+povratna vrednost - suma svih elemenata vektora, izracunata pokretanjem 2 niti 
+(svaka treba da obradi jednu polovinu elemenata)
+*/
+
 double sumiraj(vector<double> v) {
+    // Promenljive za cuvanje rezultata
     double zbir1, zbir2;
 
-    ci mid = v.begin() + v.size() / 2;
+    // Iteratori
+    ci pocetak = v.begin();
+    ci sredina = v.begin() + v.size() / 2;
+    ci kraj = v.end();
 
-    thread t1(f, v.begin(), mid, ref(zbir1));    //thread 1
-    thread t2(f, mid, v.end(), ref(zbir2));      //thread 2
+    // Pokretanje niti
+    thread t1(f, pocetak, sredina, ref(zbir1));
+    thread t2(f, sredina, kraj, ref(zbir2));
 
+    // Cekanje da se niti zavrse
     t1.join();
     t2.join();
 
