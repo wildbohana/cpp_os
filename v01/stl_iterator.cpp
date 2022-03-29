a0e6bb85c400718255816e7f2ee56c7478214d34
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:00:11 +0200

    Update stl_iterator.cpp

--- v01/stl_iterator.cpp
+++ v01/stl_iterator.cpp
@@ -11,40 +11,40 @@ using namespace std;
 
 int main() {
 	// Unos broja linija
-    int n = -1;
-    while (n < 0) {
-        cout << "Unesite broj linija koji zelite da unesete: ";
-        cin >> n;
-    }
+	int n = -1;
+	while (n < 0) {
+        	cout << "Unesite broj linija koji zelite da unesete: ";
+        	cin >> n;
+	}
 
-    vector<string> unos;
-    string temp;
+	vector<string> unos;
+	string temp;
 
-    // Ucitavanje stringova
-    cout << "Unesite linije:" << endl;
+	// Ucitavanje stringova
+	cout << "Unesite linije:" << endl;
 	for (int i = 0; i < n; i++) {
 		cout << "Linija " << i+1 << ": ";
 		cin >> temp;
 
 		// Dodavanje novog stringa u vektor
 		unos.push_back(temp);
-    }
+	}
 
-    cout << endl;
+	cout << endl;
 
-    // Ispis (koristimo obicne iteratore)
-    cout << "Ispis preko iteratora:" << endl;
-    for (auto it = unos.cbegin(); it != unos.cend(); it++) {
-        cout << *it << endl;
+	// Ispis (koristimo obicne iteratore)
+	cout << "Ispis preko iteratora:" << endl;
+	for (auto it = unos.cbegin(); it != unos.cend(); it++) {
+	        cout << *it << endl;
 	}
 
 	/*
-    cout << endl << "Ispis preko iteratora - drugi način:" << endl;
-    for (auto it : unos) {
-        cout << it << endl;
+	cout << endl << "Ispis preko iteratora - drugi način:" << endl;
+	for (auto it : unos) {
+        	cout << it << endl;
 	}
 	*/
 
-    return 0;
+	return 0;
 }
 
