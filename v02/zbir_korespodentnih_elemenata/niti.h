df3a13633524809208525f81fbee314a3125358b
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:24:53 +0200

    Update niti.h

--- v02/zbir_korespodentnih_elemenata/niti.h
+++ v02/zbir_korespodentnih_elemenata/niti.h
@@ -16,19 +16,19 @@ povratna vrednost - suma korespondentnih elemenata vektora A i B, izracunata poz
 
 vector<double> sumiraj(vector<double> a, vector<double> b) {
 	// Vektor u koji smestamo zbirove parova
-    vector<double> c (a.size());
-	
-    unsigned polovina = (a.size() % 2 == 0 ? (a.size() / 2) : (a.size() / 2) + 1);
+	vector<double> c (a.size());
+
+	unsigned polovina = (a.size() % 2 == 0 ? (a.size() / 2) : (a.size() / 2) + 1);
 
 	// void f(ci a_begin, ci a_end, ci b_begin, vector<double>::iterator sum_begin)
 	// t1 sabira korespodentne elemente u prvoj polovini, t2 sabira elemente u drugoj polovini
-    thread t1 = thread(f, a.begin(), a.begin() + polovina, b.begin(), c.begin());
-    thread t2 = thread(f, a.begin() + polovina, a.end(), b.begin() + polovina, c.begin() + polovina);
+	thread t1 = thread(f, a.begin(), a.begin() + polovina, b.begin(), c.begin());
+	thread t2 = thread(f, a.begin() + polovina, a.end(), b.begin() + polovina, c.begin() + polovina);
 
-    t1.join();
-    t2.join();
+	t1.join();
+	t2.join();
 
-    return c;
+	return c;
 }
 
 #endif // NITI_H_INCLUDED
