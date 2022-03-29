c2d8d537049be7066732c826d51f587b1c23a3de
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:21:12 +0200

    Update suma_vektora.h

--- v02/suma_vektora_Nniti/suma_vektora.h
+++ v02/suma_vektora_Nniti/suma_vektora.h
@@ -8,12 +8,13 @@ using namespace std;
 typedef vector<double>::const_iterator ci;
 
 /*
-pocetak - iterator na prvi element koji se uzima u obzir pri računanju sume
-kraj - iterator iza poslednjeg elementa koji se uzima u obzir pri računanju sume
-zbir - parameter kroz koji funkcija treba da vrati izračunatu sumu
+pocetak - iterator na prvi element koji se uzima u obzir pri racunanju sume
+kraj - iterator iza poslednjeg elementa koji se uzima u obzir pri racunanju sume
+zbir - parameter kroz koji funkcija treba da vrati izracunatu sumu
 */
-void f(ci pocetak, ci kraj, double& zbir) {
-    //double zbir = 0;
+
+void f(ci pocetak, ci kraj, double &zbir) {
+    bir = 0;
     while (pocetak != kraj) {
         zbir += *pocetak++;
     }
