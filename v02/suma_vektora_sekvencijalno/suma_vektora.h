1b54d2f62d354cf7790868af2963b96db715ba27
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:22:32 +0200

    Update suma_vektora.h

--- v02/suma_vektora_sekvencijalno/suma_vektora.h
+++ v02/suma_vektora_sekvencijalno/suma_vektora.h
@@ -2,21 +2,30 @@
 #define SUMA_VEKTORA_H_INCLUDED
 
 #include <vector>
-
 using namespace std;
 
 typedef vector<double>::const_iterator ci;
 
 /*
-pocetak - iterator na prvi element koji se uzima u obzir pri računanju sume
-kraj - iterator iza poslednjeg elementa koji se uzima u obzir pri računanju sume
-zbir - parameter kroz koji funkcija treba da vrati izračunatu sumu
+pocetak - iterator na prvi element koji se uzima u obzir pri racunanju sume
+kraj - iterator iza poslednjeg elementa koji se uzima u obzir pri racunanju sume
+zbir - parametar kroz koji funkcija treba da vrati izracunatu sumu
 */
+
 void f(ci pocetak, ci kraj, double& zbir) {
-    zbir = 0;         // ne mora, ali nam garantuje da će zbir na početku biti 0
-    while (pocetak != kraj) {
+	// Ne mora, ali nam garantuje da ce zbir na početku biti 0
+	zbir = 0;       
+    
+	while (pocetak != kraj) {
         zbir += *pocetak++;
     }
+
+	/*
+	// A moze i preko for petlje:
+	for (ci it = pocetak; it != kraj; it++) {
+        zbir += *it;
+    }
+	*/
 }
 
 #endif // SUMA_VEKTORA_H_INCLUDED
