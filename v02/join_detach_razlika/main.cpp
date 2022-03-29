b055c4fa2fabc8267c8a6ff89ddb3d1520576841
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:14:32 +0200

    Update main.cpp

--- v02/join_detach_razlika/main.cpp
+++ v02/join_detach_razlika/main.cpp
@@ -1,9 +1,9 @@
 /*
-Napraviti konkurentni program koji stvara nit iz koje pita korisnika za visinu u centimetrima,
-i potom ispisuje: “Vasa visina je <uneta_visina> cm.”
+Napraviti konkurentni program koji stvara nit iz koje pita korisnika za visinu u 
+centimetrima i potom ispisuje: “Vasa visina je <uneta_visina> cm.”
 
-Testirati program tako što će main nit, koristeći objekat koji predstavlja novu nit, da pozove metodu join.
-Potom promeniti taj poziv u poziv metode detach.
+Testirati program tako sto ce main nit, koristeci objekat koji predstavlja novu nit,
+da pozove metodu join. Potom promeniti taj poziv u poziv metode detach.
 */
 
 #include <iostream>
@@ -15,22 +15,23 @@ void f() {
     int x;
     cout << "Unesite visinu: ";
     cin >> x;
+
     cout << "Vasa visina je: " << x << "." << endl;
 }
 
 int main()
 {
     /*
-    // ovo je greška jer će napraviti dve niti koje će jedna drugoj smetati
-    thread(f).detach();     // umre odmah
-    thread(f).join();       // radi
+    // Ovo je greska jer ce napraviti dve niti koje ce jedna drugoj smetati
+    thread(f).detach();     // zavrsava program, ali ostavlja nit da radi
+    thread(f).join();       // ceka da se niz zavrsi
 
-    // ovo je manja greška ali opet je greška
+    // Ovo je manja greska, ali je opet greska
     thread(f).join();
     thread(f).detach();
     */
 
-    // ovo je najpravilnije
+    // Ovo je najpravilnije
     thread t(f);
     t.join();
 
