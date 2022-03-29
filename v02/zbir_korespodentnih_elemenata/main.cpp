047275162bd57f95cd0e968e8f8feffde5377cb7
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:23:00 +0200

    Update main.cpp

--- v02/zbir_korespodentnih_elemenata/main.cpp
+++ v02/zbir_korespodentnih_elemenata/main.cpp
@@ -1,17 +1,19 @@
 /*
-Napraviti konkurentni program (koristeći funkciju f()) koji sabira korespondentne elemente kontejnera a i b, i rezultat smešta u vektor zbir.
+Napraviti konkurentni program (koristeći funkciju f()) koji sabira korespondentne elemente
+kontejnera a i b, a rezultat smešta u vektor zbir.
 
 typedef vector<double>::const_iterator ci;
 void f(ci a_begin, ci a_end, ci b_begin, vector<double>::iterator sum_begin);
 
-Funkciju f implementirati u fajlu suma_vektora.h.
+Funkciju f implementirati u fajlu suma_vektora.cpp.
 
-Rezultat se upisuje korišćenjem iteratora sum_begin. Zato je neophodno da vektor nad kojim je kreiran taj iterator
-bude kreiran pre poziva funkcije f. Taj vektor mora da sadrži dovoljno mesta za upis svih elemenata.
+Rezultat se upisuje koriscenjem iteratora sum_begin. Zato je neophodno da vektor nad kojim je
+kreiran taj iterator bude kreiran pre poziva funkcije f. Taj vektor mora da sadrzi dovoljno mesta
+za upis svih elemenata.
 
-Funkciju sumiraj implementirati u fajlu niti.h.
-Ova funkcija treba da pokrene dve niti koje će, koristeći funkciju f, da izračunaju korespondentne sume vektora.
-I to tako da svaka nit obradi svoju polovinu elemenata (prva nit prvu polovinu, druga nit drugu polovinu).
+Funkciju sumiraj implementirati u fajlu vise_niti.h. Ova funkcija treba da pokrene dve niti
+koje ce, koristeci funkciju f, da izracunaju korespondentne sume vektora, i to tako da svaka
+nit obradi svoju polovinu elemenata (prva nit prvu polovinu, druga nit drugu polovinu).
 
 Program optimizovati za procesor sa 2 jezgra.
 
@@ -27,10 +29,11 @@ Sabiranje korespodentnih elemenata je sabiranje matrica (vektora).
 using namespace std;
 
 void testirajSumu() {
-    vector<double> a={1,2,3,4,5,6,7,8,9};
-    vector<double> b={1,2,3,4,5,6,7,8,9};
+    vector<double> a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<double> b = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     vector<double> sum(a.size());
 
+	// Sekvencijalno sumiranje
     f(a.begin(), a.end(), b.begin(), sum.begin());
 
     cout << "Suma vektora pozivom funkcije f:" << endl;
@@ -41,10 +44,11 @@ void testirajSumu() {
 }
 
 void testirajNiti() {
-    vector<double> a={1,2,3,4,5,6,7,8,9};
-    vector<double> b={1,2,3,4,5,6,7,8,9};
+    vector<double> a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<double> b = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     vector<double> sum(a.size());
 
+	// Sumiranje preko niti
     sum = sumiraj(a, b);
 
     cout << "Suma vektora, koriscenjem 2 niti: " << endl;
@@ -56,6 +60,7 @@ void testirajNiti() {
 
 int main() {
     testirajSumu();
+	
     // Otkomentarisati kada implementirate funkciju sumiraj:
     testirajNiti();
 
