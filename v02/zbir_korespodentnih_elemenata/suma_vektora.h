7c0d02449ce77c9228dcda77a64fb5ecefeeccdd
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:25:27 +0200

    Update suma_vektora.h

--- v02/zbir_korespodentnih_elemenata/suma_vektora.h
+++ v02/zbir_korespodentnih_elemenata/suma_vektora.h
@@ -8,15 +8,17 @@ using namespace std;
 typedef vector<double>::const_iterator ci;
 
 /*
-Funkcija niti. Funkcija preuzima sledeće parametre:
-1) ci a_begin -> iterator na početak dela vektora a
+Funkcija niti. Funkcija preuzima sledece parametre:
+1) ci a_begin -> iterator na pocetak dela vektora a
 2) ci a_end   -> iterator na kraj dela vektora a (dovoljan je samo jedan kraj)
-3) ci b_begin -> iterator na početak dela vektora b
-4) vector<double>::iterator sum_begin  -> iterator na početak dela vektora suma
+3) ci b_begin -> iterator na pocetak dela vektora b
+4) vector<double>::iterator sum_begin  -> iterator na pocetak dela vektora suma
 */
+
 void f(ci a_begin, ci a_end, ci b_begin, vector<double>::iterator sum_begin) {
-    for(; a_begin != a_end; a_begin++, b_begin++, sum_begin++)
+    for(; a_begin != a_end; a_begin++, b_begin++, sum_begin++) {
         *sum_begin = *a_begin + *b_begin;
+    }
 }
 
 #endif // SUMA_VEKTORA_H_INCLUDED
