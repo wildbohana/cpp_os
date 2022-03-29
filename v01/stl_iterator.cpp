38d697bfc563fe50742a67b33710d008fcc1ef09
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 21:42:48 +0200

    Update stl_iterator.cpp

--- v01/stl_iterator.cpp
+++ v01/stl_iterator.cpp
@@ -1,5 +1,6 @@
 /*
-Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i zatim ih ispisuje u redosledu kojim su uneti.
+Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza 
+i zatim ih ispisuje u redosledu kojim su uneti.
 */
 
 #include <iostream>
@@ -9,24 +10,37 @@ Napraviti program koji prihvata proizvoljan broj linija sa standardnog ulaza i z
 using namespace std;
 
 int main() {
+	// Unos broja linija
+    int n = -1;
+    while (n < 0) {
+        cout << "Unesite broj linija koji zelite da unesete: ";
+        cin >> n;
+    }
 
-    vector<string> linije;
-    string linija;
+    vector<string> unos;
+    string temp;
 
+    // Ucitavanje stringova
     cout << "Unesite linije:" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Linija " << i+1 << ": ";
+		cin >> temp;
 
-    while (getline(cin, linija)) {
-        linije.push_back(linija);
-	}
+		// Dodavanje novog stringa u vektor
+		unos.push_back(temp);
+    }
 
+    cout << endl;
+
+    // Ispis (koristimo obicne iteratore)
     cout << "Ispis preko iteratora:" << endl;
-    for (auto it = linije.begin(); it != linije.end(); it++) {
+    for (auto it = unos.cbegin(); it != unos.cend(); it++) {
         cout << *it << endl;
 	}
 
 	/*
     cout << endl << "Ispis preko iteratora - drugi način:" << endl;
-    for (auto it : linije) {
+    for (auto it : unos) {
         cout << it << endl;
 	}
 	*/
