5049db5b1fc6f1200be52abbaab1d9912c050a81
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 21:56:01 +0200

    Update stl_vector_min_n.cpp

--- v01/stl_vector_min_n.cpp
+++ v01/stl_vector_min_n.cpp
@@ -3,7 +3,7 @@ Napraviti funkciju:
 	
 vector<int> min_n(const vector<int>& v,	int n);
 
-Ova funkcija vraća vektor koji sadrži n najmanjih elemenata iz vektora v.
+Ova funkcija vraca vektor koji sadrzi n najmanjih elemenata iz vektora v.
 Podrazumeva se: v.size()>=n
 */
 
@@ -14,11 +14,16 @@ Podrazumeva se: v.size()>=n
 using namespace std;
 
 vector<int> min_n(const vector<int> &v, int n) {
-
+	// Pravimo pomocni vektor (kopija od v)
     vector<int> temp = v;
+
+	// Sortiramo pomocni vektor)
     sort(temp.begin(), temp.end());
 
+	// Pravimo novi vektor
     vector<int> pov;
+
+	// U taj vektor ubacujemo n najmanjih elemenata iz sortiranog niza
     for(int i = 0; i < n; i++) {
         pov.push_back(temp[i]);
 	}
@@ -27,14 +32,26 @@ vector<int> min_n(const vector<int> &v, int n) {
 }
 
 int main() {
-
     int niz[] = {188, 12, 1, 6, 22, 14};
+
+	// n = sizeof(niz) / sizeof(int)
+	// pocetak je niz, kraj je niz+n
     vector<int> a(niz, niz + sizeof(niz) / sizeof(int));
 
-    vector<int> b = min_n(a,3);
+    vector<int> b = min_n(a, 3);
 	
-    for(auto it : b) 
+    for(auto it : b) {
         cout << it << endl;
+	}
+
+	/*
+	// Ispis rezultata - drugi nacin
+    cout << "Rezultujuci vektor: ";
+    for (int i = 0; i < m; i++) {
+        cout << w[i] << " ";
+    }
+    cout << endl;
+	*/
     
 	return 0;
 }
