06d15dd7e399bcc25e1718abaf069b14912e3506
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-29 22:20:57 +0200

    Update niti.h

--- v02/suma_vektora_Nniti/niti.h
+++ v02/suma_vektora_Nniti/niti.h
@@ -8,10 +8,13 @@
 
 using namespace std;
 
-// v - vektor čije elemente treba sumirati
-// n - koliko niti treba pokrenuti
-// povratna vrednost - suma svih elemenata vektora, izračunata pokretanjem n niti
-// svaka nit treba da obradi jedan deo elemenata
+/*
+v - vektor cije elemente treba sumirati
+n - koliko niti treba pokrenuti
+povratna vrednost - suma svih elemenata vektora, izracunata pokretanjem n niti 
+(svaka nit treba da obradi jedan deo elemenata)
+*/
+
 double sumiraj(vector<double> v, int n) {
     thread t[n];
     vector<double> zbir(n);
@@ -19,9 +22,7 @@ double sumiraj(vector<double> v, int n) {
 
     for(int i = 0; i < n; i++) {
         ci kraj = (i < n - 1) ? pocetak + v.size() / n : v.cend();
-
         t[i] = thread(f, pocetak, kraj, ref(zbir[i]));
-
         pocetak = kraj;
     }
 
@@ -29,6 +30,7 @@ double sumiraj(vector<double> v, int n) {
 
     double suma;
     f(zbir.cbegin(), zbir.cend(), suma);
+
     return suma;
 }
 
