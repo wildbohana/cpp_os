1bcfc80851e89fe6841bbf29ece6b571c4176d15
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:39:48 +0200

    Update main.cpp

--- v02/stl_racunari/main.cpp
+++ v02/stl_racunari/main.cpp
@@ -1,7 +1,8 @@
 /* 
 Napisati program koji omogućava evidentiranje fakultetskih učionica.
 
-Za svaku učionicu potrebno je evidentirati naziv učionice, kvadraturu i sve računare koji se nalaze u učionici. 
+Za svaku učionicu potrebno je evidentirati naziv učionice, kvadraturu 
+i sve računare koji se nalaze u učionici. 
 Za svaki računar evidentiraju se podaci o hard disku i procesoru.
 
 U okviru main funkcije, definisati jednu učionicu koja sadrži dva računara.
