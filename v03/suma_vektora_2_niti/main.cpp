3451c8b3c08bebf04d83d8db8d0a21398a4fc825
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:42:39 +0200

    Update main.cpp

--- v03/suma_vektora_2_niti/main.cpp
+++ v03/suma_vektora_2_niti/main.cpp
@@ -1,14 +1,19 @@
 /*
-Napraviti konkurentni program koji izračunava sumu svih elemenata vektora, koristeći funkciju f():
+Napraviti konkurentni program koji izračunava sumu svih elemenata vektora, 
+koristeći funkciju f():
 
 typedef vector<double>::const_iterator ci;
 void f(ci pocetak, ci kraj, double& zbir);
 
 Funkcija f nalazi se u fajlu suma_vektora.h.
 
-Potrebno je implementirati i funkciju sumiraj (fajl niti.h) koja treba da pokrene dve niti i da svakoj niti prosledi polovinu elemenata ulaznog vektora. Obe niti treba da koriste funkciju f kako bi izračunale sumu svog dela vektora.
+Potrebno je implementirati i funkciju sumiraj (fajl niti.h) 
+koja treba da pokrene dve niti i da svakoj niti prosledi polovinu 
+elemenata ulaznog vektora. Obe niti treba da koriste funkciju f 
+kako bi izračunale sumu svog dela vektora.
 
-Napomena: ovakva optimizacija sumiranja je značajna kada se radi na dvojezgarnom procesoru za vektore velike dužine.
+Napomena: ovakva optimizacija sumiranja je značajna kada se 
+radi na dvojezgarnom procesoru za vektore velike dužine.
 */
 
 #include <iostream>
