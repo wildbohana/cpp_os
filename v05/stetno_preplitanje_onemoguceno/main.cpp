15a4ff71238b85d4a72bc48ce963d54878965915
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:38:18 +0200

    Update main.cpp

--- v05/stetno_preplitanje_onemoguceno/main.cpp
+++ v05/stetno_preplitanje_onemoguceno/main.cpp
@@ -1,5 +1,6 @@
 /*
-Napraviti konkurentni program koji u funkciji visina pita korisnika koliko je visok. Nakon toga korisnik unosi svoju visinu. Na kraju u funkciji se ispisuje uneta visina.
+Napraviti konkurentni program koji u funkciji visina pita korisnika koliko je visok. 
+Nakon toga korisnik unosi svoju visinu. Na kraju u funkciji se ispisuje uneta visina.
 
 Sprečiti štetno preplitanje na terminalu korišćenjem klase mutex.
 
