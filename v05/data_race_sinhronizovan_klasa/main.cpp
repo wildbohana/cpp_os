5c714d3617710b27fc267d9f36e92f464f309b32
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:35:44 +0200

    Update main.cpp

--- v05/data_race_sinhronizovan_klasa/main.cpp
+++ v05/data_race_sinhronizovan_klasa/main.cpp
@@ -8,13 +8,16 @@ class Brojac {
       friend ostream& operator<<(ostream& , Brojac& );
 };
 
-Metode inc i dec povećavaću i smanjuju vrednost brojača respektivno. Operator << služi za ispis brojača na ekran.
+Metode inc i dec povećavaću i smanjuju vrednost brojača respektivno. 
+Operator << služi za ispis brojača na ekran.
 
 Klasa treba da zaštiti konzistentnost brojača u datim metodama uz pomoć klasa mutex i unique_lock. 
 
 Kreirati 1 globalni objekat brojača kome će pristupati 2 niti. 
 
-Kreirati 2 niti pri čemu jedna nit poziva metodu uvećavanja brojača 1000000 puta a druga metodu smanjivanja brojača 1000000 puta. Na kraju programa ispisati konačnu vrednost brojača nakon uvećavanja i smanjivanja.
+Kreirati 2 niti pri čemu jedna nit poziva metodu uvećavanja brojača 
+1000000 puta a druga metodu smanjivanja brojača 1000000 puta. 
+Na kraju programa ispisati konačnu vrednost brojača nakon uvećavanja i smanjivanja.
 */
 
 #include <iostream>
@@ -77,6 +80,7 @@ int main()
 	t1.join();
 	t2.join();
 
+	// Dobije se 0 posle pola godine izvrsavanja
 	cout << br;
 
 	return 0;
