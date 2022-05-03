3b5bb05d02d95aa6379144fa6b5ff8f701c9d1a2
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:37:16 +0200

    Update main.cpp

--- v05/mutex_exception/main.cpp
+++ v05/mutex_exception/main.cpp
@@ -1,5 +1,8 @@
 /*
-Napraviti konkurentni program u kom se u funkciji niti pravi veliki niz od 1.000.000.000.000 elemenata. Niz se pravi dinamički. Kreiranje niza zaštititi try - catch blokom. U okviru try catch bloka zaključati mutex pre pravljenja niza i otključati ga nakon pravljenja niza. 
+Napraviti konkurentni program u kom se u funkciji niti pravi veliki niz 
+od 1.000.000.000.000 elemenata. Niz se pravi dinamički. 
+Kreiranje niza zaštititi try - catch blokom. 
+U okviru try catch bloka zaključati mutex pre pravljenja niza i otključati ga nakon pravljenja niza. 
 
 Posmatrati ponašanje programa.
 
