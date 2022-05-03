cbed4a28669ab1c475b2c5de3ac7c1cc6423828f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-03 23:33:41 +0200

    Update main.cpp

--- v05/data_race/main.cpp
+++ v05/data_race/main.cpp
@@ -1,12 +1,14 @@
 /*
-Kreirati globalnu celobrojnu promeljivu brojač. Nakon toga kreirati 2 funkcije inkrement i dekrement koje povećavaju i smanjuju dati brojač ITERACIJA puta. ITERACIJA je konstanta koja predstavlja proizvoljno velik broj (npr. 100.000.000). 
+Kreirati globalnu celobrojnu promeljivu brojač. Nakon toga kreirati 2 funkcije 
+inkrement i dekrement koje povećavaju i smanjuju dati brojač ITERACIJA puta. 
+ITERACIJA je konstanta koja predstavlja proizvoljno velik broj (npr. 100.000.000). 
 
 Kreirati jednu nit od funkcije inkrement i jednu nit od funkcije dekrement.
 
 Nakon završetka rada niti ispisati vrednosti brojača. Da li je data vrednost očekivana? 
 */
 
-// BRO THIS FILE WAS SKROZ EMPTY !
+// BRO THIS FILE WAS SKROZ EMPTY
 
 #include <iostream>
 #include <thread>
@@ -37,6 +39,7 @@ int main()
 	t1.join();
 	t2.join();
 
+	// Necemo dobiti 0 nego neku random vrednost
 	cout << brojac << endl;
 
 	return 0;
