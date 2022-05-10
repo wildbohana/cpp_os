a4a129d6b880c6c54a15ae934d1148a221be30d2
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:22:58 +0200

    Update posta.h

--- t34/posta/posta.h
+++ t34/posta/posta.h
@@ -13,60 +13,56 @@ class Posta
 {
 	private:
 		Klijent& klijent;
-		// Dodato:
-		int uplaceno[2];
-		bool salterZauzet[2];
-		condition_variable cv;
 		mutex m;
+		condition_variable cv;
+		int uplaceno[2];
+		bool slobodan[2];
+
 	public:
 		// Prosiriti po potrebi ...
 		Posta(Klijent& kl) : klijent(kl) 
 		{
+			slobodan[0] = true;
+			slobodan[1] = true;
 			uplaceno[0] = 0;
 			uplaceno[1] = 0;
-			salterZauzet[0] = false;
-			salterZauzet[1] = false;
 		}
 
-		/*
-		Metoda koju poziva nit koja simulira klijenta kada on uplacuje neki iznos.
-		Metoda je blokirajuca - ako je salter zauzet, metoda ce stajati blokirana dok se on ne oslobodi.
+		// Metoda koju poziva nit koja simulira klijenta kada on uplacuje neki iznos.
+		// Metoda je blokirajuca - ako je salter zauzet, metoda ce stajati blokirana dok se on ne oslobodi.
+		//
+		// rbr   - Redni broj klijenta
+		// svota - Svota koju klijent zeli da uplati
+		//
+		// Potrebno je pozvati metodu klijent.ceka kada su salteri zauzeti i klijent mora da ceka.
+		// Potrebno je pozvati metodu klijent.uplacuje kada klijent stupi na salter i vrsi svoje placanje.
+		// Potrebno je pozvati metodu klijent.napusta kada klijent zavrsi placanje i napusta salter.
 		
-		rbr   - Redni broj klijenta
-		svota - Svota koju klijent zeli da uplati
-		
-		Potrebno je pozvati metodu klijent.ceka kada su salteri zauzeti i klijent mora da ceka.
-		Potrebno je pozvati metodu klijent.uplacuje kada klijent stupi na salter i vrsi svoje placanje.
-		Potrebno je pozvati metodu klijent.napusta kada klijent zavrsi placanje i napusta salter.
-		*/
-
 		// Implementirati ...
 		void uplati(int rbr, int svota) 
 		{
 			unique_lock<mutex> l(m);
 
-			// ako su oba saltera zauzeta - cekaj
-			while (salterZauzet[0] && salterZauzet[1])
+			while (!slobodan[0] && !slobodan[1])
 			{
 				klijent.ceka(rbr, svota);
 				cv.wait(l);
 			}
-			
-			// pretty genius if u ask me
-			// ako je salter[0] zauzet, selektuj salter[1], ako je slobodan, selektuj salter[0]
-			int salter = salterZauzet[0] ? 1 : 0; 
-			// zauzmi selektovani salter
-			salterZauzet[salter] = true;
+
+			int salter = (slobodan[0] ? 0 : 1);
+			slobodan[salter] = false;
 
 			klijent.uplacuje(rbr, salter, svota);
 
 			l.unlock();
-			this_thread::sleep_for(seconds(svota));
+			this_thread::sleep_for(chrono::seconds(svota));
 			l.lock();
 
 			uplaceno[salter] += svota;
-			salterZauzet[salter] = false;
+
+			slobodan[salter] = true;
 			klijent.napusta(rbr, salter, uplaceno[salter]);
+
 			cv.notify_one();
 		}
 };
