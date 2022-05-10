3171912bc1021d3d3c489a1d9e305ece65a67384
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:33:33 +0200

    Update trzni_centar.h

--- t34/trzni_centar/trzni_centar.h
+++ t34/trzni_centar/trzni_centar.h
@@ -27,15 +27,17 @@ class Trzni_centar
 			cekaju[1] = 0;
 		}
 
-		// Metoda koju poziva nit koja simulira kupca kako bi se obavila kupovina.
-		// Metoda je blokirajuca - ako su sve kase zauzete, onda se izvrsenje blokira dok se neka ne oslobodi.
-		//
-		// rbr           - Redni broj kupca
-		// broj_artikala - Broj artikala koje kupac zeli da kupi
-		//
-		// Potrebno je pozvati metodu kupac.ceka kada je izabrana kasa zauzeta i kupac mora da ceka.
-		// Potrebno je pozvati metodu kupac.kupuje kada se kasa oslobodi i kupac pocinje kupovinu.
-		// Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio kupovinu.
+		/*
+		Metoda koju poziva nit koja simulira kupca kako bi se obavila kupovina.
+		Metoda je blokirajuca - ako su sve kase zauzete, onda se izvrsenje blokira dok se neka ne oslobodi.
+		
+		rbr           - Redni broj kupca
+		broj_artikala - Broj artikala koje kupac zeli da kupi
+		
+		Potrebno je pozvati metodu kupac.ceka kada je izabrana kasa zauzeta i kupac mora da ceka.
+		Potrebno je pozvati metodu kupac.kupuje kada se kasa oslobodi i kupac pocinje kupovinu.
+		Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio kupovinu.
+		*/
 		
 		// Implementirati ...
 		int kupi(int rbr, int broj_artikala) 
