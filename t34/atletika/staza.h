f05192a09f5ec325e2a742a1edc92aed27773e1a
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:27:07 +0200

    Update staza.h

--- t34/atletika/staza.h
+++ t34/atletika/staza.h
@@ -32,15 +32,17 @@ class Atletska_staza
 			bacaca_ceka = 0;
 		}
 
-		// Metoda koju poziva nit koja simulira skakaca kako bi takmicar obavio skok.
-		// Ukoliko je zaletiste zauzeto, ova metoda ce stajati blokirana dok se ono ne oslobodi i takmicar obavi nastup.
-		//
-		// rbr - Redni broj takmicara
-		//
-		// Potrebno je pozvati metodu takmicar.skakac_ceka kada skakac ne moze da stupi na zaletiste.
-		// Potrebno je pozvati metodu takmicar.skakac_skace kada skakac stupi na zaletiste, pre skoka.
-		// Potrebno je pozvati metodu takmicar.skakac_zavrsio kada je skakac zavrsio i kada treba prikazati njegov rezultat.
+		/*
+		Metoda koju poziva nit koja simulira skakaca kako bi takmicar obavio skok.
+		Ukoliko je zaletiste zauzeto, ova metoda ce stajati blokirana dok se ono ne oslobodi i takmicar obavi nastup.
 		
+		rbr - Redni broj takmicara
+		
+		Potrebno je pozvati metodu takmicar.skakac_ceka kada skakac ne moze da stupi na zaletiste.
+		Potrebno je pozvati metodu takmicar.skakac_skace kada skakac stupi na zaletiste, pre skoka.
+		Potrebno je pozvati metodu takmicar.skakac_zavrsio kada je skakac zavrsio i kada treba prikazati njegov rezultat.
+		*/
+			
 		// Implementirati ...
 		Povratna_vrednost skaci(int rbr) 
 		{
@@ -88,14 +90,16 @@ class Atletska_staza
 			return pv;
 		}
 
-		// Metoda koju poziva nit koja simulira bacaca kako bi takmicar obavio bacanje.
-		// Ukoliko je zaletiste zauzeto, ova metoda ce stajati blokirana dok se ono ne oslobodi i takmicar obavi nastup.
-		//
-		// rbr - Redni broj takmicara
-		//
-		// Potrebno je pozvati metodu takmicar.bacac_ceka kada bacac ne moze da stupi na zaletiste.
-		// Potrebno je pozvati metodu takmicar.bacac_skace kada bacac stupi na zaletiste, pre skoka.
-		// Potrebno je pozvati metodu takmicar.bacac_zavrsio kada je bacac zavrsio i kada treba prikazati njegov rezultat.
+		/*
+		Metoda koju poziva nit koja simulira bacaca kako bi takmicar obavio bacanje.
+		Ukoliko je zaletiste zauzeto, ova metoda ce stajati blokirana dok se ono ne oslobodi i takmicar obavi nastup.
+		
+		rbr - Redni broj takmicara
+		
+		Potrebno je pozvati metodu takmicar.bacac_ceka kada bacac ne moze da stupi na zaletiste.
+		Potrebno je pozvati metodu takmicar.bacac_skace kada bacac stupi na zaletiste, pre skoka.
+		Potrebno je pozvati metodu takmicar.bacac_zavrsio kada je bacac zavrsio i kada treba prikazati njegov rezultat.
+		*/
 		
 		// Implementirati ...
 		Povratna_vrednost baciKoplje(int rbr) 
