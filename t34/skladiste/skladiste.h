44ac7c0b5d4e392bb5f425a35f5548cfe56a9541
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:31:41 +0200

    Update skladiste.h

--- t34/skladiste/skladiste.h
+++ t34/skladiste/skladiste.h
@@ -27,17 +27,19 @@ class Skladiste
 			zauzeta_rampa[1] = false;
 		}
 
-		// Metoda koju poziva nit koja simulira kretanje kamiona kada on pokusava da istovari robu.
-		// Metoda je blokirajuca - ako su sve rampe za istovar zauzete, izvrsenje ce blokirati dok se neka ne oslobodi.
-		//
-		// rbr       - Redni broj kamiona
-		// kolicina  - Kolicina robe koja se prevozi (u tonama)
-		// zapaljivo - Ukazuje na to da li je roba koja se prevozi zapaljiva (takva roba ima prioritet pri istovaru!)
-		//
-		// Potrebno je pozvati metodu kamion.ceka kada su rampe zauzete i kamion mora da ceka.
-		// Potrebno je pozvati metodu kamion.istovara kada zapocne istovar robe iz kamiona (primetiti da ta metoda zahteva da joj se prosledi indeks rampe na kojoj se obavlja istovar!)
-		// Potrebno je pozvati metodu kamion.odlazi kada je kamion zavrsio istovar i odlazi.
-
+		/*
+		Metoda koju poziva nit koja simulira kretanje kamiona kada on pokusava da istovari robu.
+		Metoda je blokirajuca - ako su sve rampe za istovar zauzete, izvrsenje ce blokirati dok se neka ne oslobodi.
+		
+		rbr       - Redni broj kamiona
+		kolicina  - Kolicina robe koja se prevozi (u tonama)
+		zapaljivo - Ukazuje na to da li je roba koja se prevozi zapaljiva (takva roba ima prioritet pri istovaru!)
+		
+		Potrebno je pozvati metodu kamion.ceka kada su rampe zauzete i kamion mora da ceka.
+		Potrebno je pozvati metodu kamion.istovara kada zapocne istovar robe iz kamiona (primetiti da ta metoda zahteva da joj se prosledi indeks rampe na kojoj se obavlja istovar!)
+		Potrebno je pozvati metodu kamion.odlazi kada je kamion zavrsio istovar i odlazi.
+		*/
+	
 		void istovari(int rbr, int kolicina, bool zapaljivo) 
 		{
 			unique_lock<mutex> l(m);
