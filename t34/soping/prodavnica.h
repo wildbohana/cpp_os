302a0590853c3939fc49d91f369eb03ab275ce57
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:24:07 +0200

    Update prodavnica.h

--- t34/soping/prodavnica.h
+++ t34/soping/prodavnica.h
@@ -13,53 +13,54 @@ class Prodavnica
 {
 	private:
 		Kupac& kupac;
-		// Dodato:
-		int slobodnihMesta;
 		mutex m;
 		condition_variable cv;
+		int slobodnih_kabina;		
 	public:
 		// Prosiriti po potrebi ...
 		Prodavnica(Kupac& k, int n): kupac(k) 
 		{
-			slobodnihMesta = n;
+			slobodnih_kabina = n;
 		}
 
-		/*
-		Metoda koju poziva nit koja simulira kupca, kada kupac obavlja soping (procedura je opisana u zaglavlju main.cpp)
-		Metoda je blokirajuca - ako kupac mora da ceka da se oslobodi kabina za probu, izvrsenje ce blokirati dok se za to ne steknu uslovi.
+		// Metoda koju poziva nit koja simulira kupca, kada kupac obavlja soping (procedura je opisana u zaglavlju main.cpp)
+		// Metoda je blokirajuca - ako kupac mora da ceka da se oslobodi kabina za probu, izvrsenje ce blokirati dok se za to ne steknu uslovi.
+		//
+		// rbr - Redni broj kupca
+		//
+		// Potrebno je pozvati metodu kupac.ceka kada su kabine zauzete i kupac mora da ceka.
+		// Potrebno je pozvati metodu kupac.proba_odecu kada kupac udje u kabinu da proba odecu.
+		// Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio probu odece.
 		
-		rbr - Redni broj kupca
-		
-		Potrebno je pozvati metodu kupac.ceka kada su kabine zauzete i kupac mora da ceka.
-		Potrebno je pozvati metodu kupac.proba_odecu kada kupac udje u kabinu da proba odecu.
-		Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio probu odece.
-		*/
-
 		// Implementirati ...
 		povratna_vrednost kupi(int rbr) 
 		{
+			unique_lock<mutex> l(m);
+
+			srand(time(NULL));
 			povratna_vrednost pv;
 
-			unique_lock<mutex> l(m);
 			system_clock::time_point dosao = system_clock::now();
 
-			while (slobodnihMesta == 0)
+			while (slobodnih_kabina == 0)
 			{
 				kupac.ceka(rbr);
 				cv.wait(l);
 			}
 
 			pv.cekao_na_kabinu = system_clock::now() - dosao;
-			slobodnihMesta--;
 
+			slobodnih_kabina--;
 			kupac.proba_odecu(rbr);
 
 			l.unlock();
 			this_thread::sleep_for(seconds(1));
 			l.lock();
 
-			pv.kupio = rand() % 2;
-			slobodnihMesta++;
+			slobodnih_kabina++;
+
+			pv.kupio = rand()%2;
+
 			kupac.zavrsio(rbr, pv);
 			cv.notify_one();
 
