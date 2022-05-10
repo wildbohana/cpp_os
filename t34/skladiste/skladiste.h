98b815f6c187d594440927676e3e42a6c8ebe535
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:23:43 +0200

    Update skladiste.h

--- t34/skladiste/skladiste.h
+++ t34/skladiste/skladiste.h
@@ -6,8 +6,6 @@
 #include <condition_variable>
 #include <chrono>
 
-// UOPŠTE NISAM SIGURNA DA LI OVO VALJA ALI ŠTA JE TU JE
-
 using namespace std;
 
 class Skladiste 
@@ -15,74 +13,68 @@ class Skladiste
 	private:
 		Kamion& kamion;
 		mutex m;
-		condition_variable normal;
-		condition_variable boom;
-		// Dodato:
-		bool rampa_slobodna[2];
+		condition_variable cv_boom;
+		condition_variable cv_normal;
 		int zapaljivih_ceka;
-
+		bool zapaljiv;
+		bool zauzeta_rampa[2];
 	public:
 		// Prosiriti po potrebi ...
-		Skladiste(Kamion& k) : kamion(k), zapaljivih_ceka(0)
+		Skladiste(Kamion& k) : kamion(k) 
 		{
-			rampa_slobodna[0] = true;
-			rampa_slobodna[1] = true;
+			zapaljivih_ceka = 0;
+			zauzeta_rampa[0] = false;
+			zauzeta_rampa[1] = false;
 		}
 
-		/*
-		Metoda koju poziva nit koja simulira kretanje kamiona kada on pokusava da istovari robu.
-		Metoda je blokirajuca - ako su sve rampe za istovar zauzete, izvrsenje ce blokirati dok se neka ne oslobodi.
-		
-		rbr       - Redni broj kamiona
-		kolicina  - Kolicina robe koja se prevozi (u tonama)
-		zapaljivo - Ukazuje na to da li je roba koja se prevozi zapaljiva (takva roba ima prioritet pri istovaru!)
-		
-		Potrebno je pozvati metodu kamion.ceka kada su rampe zauzete i kamion mora da ceka.
-		Potrebno je pozvati metodu kamion.istovara kada zapocne istovar robe iz kamiona (primetiti da ta metoda zahteva 
-		da joj se prosledi indeks rampe na kojoj se obavlja istovar!)
-		Potrebno je pozvati metodu kamion.odlazi kada je kamion zavrsio istovar i odlazi.
-		*/
+		// Metoda koju poziva nit koja simulira kretanje kamiona kada on pokusava da istovari robu.
+		// Metoda je blokirajuca - ako su sve rampe za istovar zauzete, izvrsenje ce blokirati dok se neka ne oslobodi.
+		//
+		// rbr       - Redni broj kamiona
+		// kolicina  - Kolicina robe koja se prevozi (u tonama)
+		// zapaljivo - Ukazuje na to da li je roba koja se prevozi zapaljiva (takva roba ima prioritet pri istovaru!)
+		//
+		// Potrebno je pozvati metodu kamion.ceka kada su rampe zauzete i kamion mora da ceka.
+		// Potrebno je pozvati metodu kamion.istovara kada zapocne istovar robe iz kamiona (primetiti da ta metoda zahteva da joj se prosledi indeks rampe na kojoj se obavlja istovar!)
+		// Potrebno je pozvati metodu kamion.odlazi kada je kamion zavrsio istovar i odlazi.
 
 		void istovari(int rbr, int kolicina, bool zapaljivo) 
 		{
+			unique_lock<mutex> l(m);
+
+			while (zauzeta_rampa[0] && zauzeta_rampa[1])
 			{
-				unique_lock<mutex> l(m);
-
-				while (rampa_slobodna[0] == false && rampa_slobodna[1] == false) 
-				{ 
+				if (zapaljivo)
+				{
+					zapaljivih_ceka++;
 					kamion.ceka(rbr, kolicina, zapaljivo);
-
-					if (zapaljivo) 
-					{ 
-						zapaljivih_ceka++;
-						boom.wait(l);
-						zapaljivih_ceka--; 
-					}
-					else
-						normal.wait(l);
+					cv_boom.wait(l);
+					zapaljivih_ceka--;
+				}
+				else
+				{
+					kamion.ceka(rbr, kolicina, zapaljivo);
+					cv_normal.wait(l);
 				}
 			}
 
-			int rampa = rampa_slobodna[0] ? 0 : 1;
-      		rampa_slobodna[rampa] = false; 
+			int rampa = zauzeta_rampa[0] ? 1 : 0;
+			zauzeta_rampa[rampa] = true;
 
 			kamion.istovara(rbr, kolicina, zapaljivo, rampa);
-			
-			// istovar traje onoliko milisekundi koliko ima tona
-			// zato što se nalazi izvan bloka sa unique_lock, ne treba ti l.unlock() i l.lock();
-			this_thread::sleep_for(chrono::milliseconds(kolicina * 1000));
 
-			{
-				unique_lock<mutex> l(m);
+			l.unlock();
+			this_thread::sleep_for(chrono::seconds(kolicina));
+			l.lock();
 
-				// oslobodi rampu nakon istovara
-				rampa_slobodna[rampa] = true; 
-				
-				if (zapaljivih_ceka) 
-					boom.notify_one();
-				else
-					normal.notify_one();
-			}
+			zauzeta_rampa[rampa] = false;
+
+			kamion.odlazi(rbr);
+
+			if (zapaljivih_ceka)
+				cv_boom.notify_one();
+			else
+				cv_normal.notify_one();
 		}
 };
 
