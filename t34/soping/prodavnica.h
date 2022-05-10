0118474dfa375990527b258ac08228241eb0f085
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-10 13:32:18 +0200

    Update prodavnica.h

--- t34/soping/prodavnica.h
+++ t34/soping/prodavnica.h
@@ -23,14 +23,16 @@ class Prodavnica
 			slobodnih_kabina = n;
 		}
 
-		// Metoda koju poziva nit koja simulira kupca, kada kupac obavlja soping (procedura je opisana u zaglavlju main.cpp)
-		// Metoda je blokirajuca - ako kupac mora da ceka da se oslobodi kabina za probu, izvrsenje ce blokirati dok se za to ne steknu uslovi.
-		//
-		// rbr - Redni broj kupca
-		//
-		// Potrebno je pozvati metodu kupac.ceka kada su kabine zauzete i kupac mora da ceka.
-		// Potrebno je pozvati metodu kupac.proba_odecu kada kupac udje u kabinu da proba odecu.
-		// Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio probu odece.
+		/*
+		Metoda koju poziva nit koja simulira kupca, kada kupac obavlja soping (procedura je opisana u zaglavlju main.cpp)
+		Metoda je blokirajuca - ako kupac mora da ceka da se oslobodi kabina za probu, izvrsenje ce blokirati dok se za to ne steknu uslovi.
+		
+		rbr - Redni broj kupca
+		
+		Potrebno je pozvati metodu kupac.ceka kada su kabine zauzete i kupac mora da ceka.
+		Potrebno je pozvati metodu kupac.proba_odecu kada kupac udje u kabinu da proba odecu.
+		Potrebno je pozvati metodu kupac.zavrsio kada je kupac zavrsio probu odece.
+		*/
 		
 		// Implementirati ...
 		povratna_vrednost kupi(int rbr) 
